92390e95624966e644635f1793a7a41385576b75
VK <belsynth@gmail.com>
2021-02-10 00:17:48 +0300

    Refactor Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -7,44 +7,39 @@ const MAX_ROUNDS_COUNT = 3;
 use function cli\prompt;
 use function cli\line;
 
-function run(string $game): void
+function run(string $game, string $goal): void
 {
     $gameNamespace = "\BrainGames\Games\\$game";
-
-    $getGoal = "$gameNamespace\getGameGoal";
     $getData = "$gameNamespace\getGameData";
 
-    if (is_callable($getGoal) && is_callable($getData)) {
-        $goal = $getGoal();
-        $gameData = $getData();
+    if (is_callable($getData)) {
+        line('Welcome to the Brain Game!');
+        $playerName = prompt('May I have your name?', '', ' ');
+        line("Hello, %s!", $playerName);
+        line($goal);
+
+        $rightAnswersCount = 0;
+
+        for ($i = 0; $i < MAX_ROUNDS_COUNT; $i++) {
+            $gameData = $getData();
+            line("Question: $gameData[0]");
+            $playersAnswer = prompt('Your answer', '');
+
+            if ($playersAnswer === $gameData[1]) {
+                line('Correct!');
+                $rightAnswersCount++;
+            } else {
+                line("'$playersAnswer' is wrong answer ;(. Correct answer was '$gameData[1]'.");
+                break;
+            }
+        }
+
+        if ($rightAnswersCount === MAX_ROUNDS_COUNT) {
+            line("Congratulations, $playerName!");
+        } else {
+            line("Let's try again, $playerName!");
+        }
     } else {
         exit('Oops! Something goes wrong...');
-    }
-
-    line('Welcome to the Brain Game!');
-    $playerName = prompt('May I have your name?', '', ' ');
-    line("Hello, %s!", $playerName);
-    line($goal);
-
-    $rightAnswersCount = 0;
-
-    for ($i = 0; $i < MAX_ROUNDS_COUNT; $i++) {
-        $questionAndAnswer = $gameData[$i];
-        line("Question: $questionAndAnswer[0]");
-        $playersAnswer = prompt('Your answer', '');
-
-        if ($playersAnswer === $questionAndAnswer[1]) {
-            line('Correct!');
-            $rightAnswersCount++;
-        } else {
-            line("'$playersAnswer' is wrong answer ;(. Correct answer was '$questionAndAnswer[1]'.");
-            break;
-        }
-    }
-
-    if ($rightAnswersCount === MAX_ROUNDS_COUNT) {
-        line("Congratulations, $playerName!");
-    } else {
-        line("Let's try again, $playerName!");
     }
 }
